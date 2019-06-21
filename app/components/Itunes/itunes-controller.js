dafbf76534f9bc129ff567618ffbb6171b56d6b1
benffrost <benffrost@gmail.com>
2019-06-21 13:19:46 -0600

    pt1 functional; maybe part 2?

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -4,6 +4,30 @@ const itunesService = new ItunesService()
 
 function drawSongs() {
   //changes button back to GET MUSIC once songs are loaded
+  let songElement = document.querySelector("#song-list")
+  let songs = itunesService.Songs;
+  let template = "";
+  for (let i = 0; i < songs.length; i++) {
+    template += `
+            <li class="row border mt-1 py-1">
+                <div class="col-2"><img class="img img-fluid" src="${songs[i].albumArt}" alt="" ></div>
+                <div class="col-5 text-left">
+                    <h5>${songs[i].artist}</h5>
+                    <h3>${songs[i].title}</h3>
+                    <h5>${songs[i].collection}</h5>
+                    <p>${songs[i].price}</p>
+                </div>
+                <div class="col-5 text-right d-flex align-items-center justify-content-end">
+                <div><audio controls>
+                <source src="${songs[i].preview}">
+                </audio></div>
+
+                </div>
+            </li>`
+  }
+
+  songElement.innerHTML = template;
+
   document.querySelector('#get-music-button').textContent = 'GET MUSIC'
   console.log(itunesService.Songs)
 
@@ -13,7 +37,8 @@ function drawSongs() {
 //PUBLIC
 class ItunesController {
   constructor() {
-    //BE SURE TO REGISTER YOUR SUBSCRIBERS!!!!!!!
+    itunesService.addSubscriber("songs", drawSongs)
+    drawSongs()
   }
 
 
